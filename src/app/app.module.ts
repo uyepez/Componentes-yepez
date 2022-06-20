a73fc767a6fda985ed5b31b98f3e4df95ec56283
uyepez <uyepez@tvp.mx>
2022-06-19 21:34:18 -0500

    componentes-yepez
    destinos turisticos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
